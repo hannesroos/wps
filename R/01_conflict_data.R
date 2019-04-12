de5b1ed834044786d72b128b6e4e2da7605e5710
hannesroos <hannesroos@hcss.nl>
2019-04-12 15:29:01 +0200

    ACLED independent variables done, write out file

--- R/01_conflict_data.R
+++ R/01_conflict_data.R
@@ -1,5 +1,7 @@
 rm(list = ls())
 
+# Front matter
+
 ## Load packages
 
 library(tidyverse)
@@ -46,6 +48,8 @@ acled <- dbGetQuery(con, "select * from ev.acled") %>%
     adm2_code = geo_inside(longitude, latitude, polygons, GID_2)
   )
 
+# Dependent variable work
+
 ## Filter ACLED data based on task, aggregate up to ADM2-month level
 
 acled_filtered <- acled %>% 
@@ -54,9 +58,9 @@ acled_filtered <- acled %>%
     !is.na(adm2_code)
   ) %>% 
   mutate(
-    month_conflict = ymd(paste(year(event_date), month(event_date), 1))
+    month_dependent = ymd(paste(year(event_date), month(event_date), 1))
   ) %>% 
-  group_by(iso3c, adm2_code, month_conflict) %>% 
+  group_by(iso3c, adm2_code, month_dependent) %>% 
   summarise(
     fatalities = sum(fatalities),
     count = n()
@@ -77,7 +81,7 @@ acled_start <- read_csv("data/acled_start_month_apr19_version.csv") %>%
 ## Create monthly rack
 
 months = tibble(
-  month_conflict = seq(ymd('1997-01-01'), ymd('2019-04-01'), by = 'month')
+  month_dependent = seq(ymd('1997-01-01'), ymd('2019-04-01'), by = 'month')
 )
 
 ## Create rack of ADM2 regions
@@ -91,7 +95,10 @@ gid2s <- polygons_df %>%
 ## Combine data on ACLED start dates per country and ADM2 regions of the countries
 
 rack <- merge(acled_start, months) %>% 
-  filter(month_conflict >= month_start) %>% 
+  mutate(
+    month_indep = month_dependent - months(1)
+  ) %>% 
+  filter(month_dependent >= month_start) %>% 
   left_join(gid2s)
 
 ## Join rack with ACLED data, create a binary indicator of conflict with fatalities in ADM2, calculate 12 month variables
@@ -102,7 +109,7 @@ acled_full <- left_join(rack, acled_filtered) %>%
     count = ifelse(is.na(count), 0, count),
     binary = ifelse(fatalities > 0, 1, 0)
   ) %>% 
-  arrange(adm2_code, month_conflict) %>% 
+  arrange(adm2_code, month_dependent) %>% 
   group_by(adm2_code) %>% 
   mutate(
     fatalities_12m = rollsum(fatalities, 12, align = "left", fill = NA),
@@ -110,3 +117,74 @@ acled_full <- left_join(rack, acled_filtered) %>%
     binary_12m = rollmax(binary, 12, align = "left", fill = NA)
   ) %>% 
   ungroup()
+
+# Independent variable work
+
+## Ftiler data, aggregate to ADM2-month level, calculate number of fatalities and count of events per group
+
+acled_indep <- acled %>% 
+  mutate(
+    month_indep = ymd(paste(year(event_date), month(event_date), 1))
+    ) %>% 
+  filter(event_type %in% c("Battles", "Explosions/remote violence", "Explosions/Remote violence", "Violence against civilians", 
+                           "Riots", "Protests") | sub_event_type  == "Agreement", !is.na(adm2_code)) %>% 
+  mutate(
+    wps_input = ifelse(event_type %in% c("Battles", "Violence against civilians"), event_type, 
+                       ifelse(event_type %in% c("Explosions/remote violence", "Explosions/Remote violence"), "Remote violence", 
+                              ifelse(sub_event_type %in% c(
+                                "Mob violence", "Violent demonstration", "Excessive force against protesters"), 
+                                "Violent protests and riots", 
+                                     ifelse(
+                                       sub_event_type %in% c("Peaceful Protest", "Protest with intervention", "Peaceful protest"), 
+                                       "Peaceful protests and riots", 
+                                       ifelse(sub_event_type == "Agreement", "Agreement", "Error"))))),
+    wps_input = gsub(" ", "_", wps_input)) %>% 
+  select(iso3c, adm2_code, month_indep, wps_input, fatalities) %>% 
+  group_by(iso3c, adm2_code, month_indep, wps_input) %>% 
+  summarise(
+    fatalities = sum(fatalities),
+    count = n()
+  ) %>% 
+  ungroup()
+
+## 
+
+wps_inputs <- distinct(acled_indep, wps_input)
+
+acled_indep_full <- merge(rack, wps_inputs) %>% 
+  left_join(acled_indep) %>% 
+  mutate(
+    fatalities = ifelse(is.na(fatalities), 0, fatalities),
+    count = ifelse(is.na(count), 0, count),
+    binary = ifelse(fatalities > 0, 1, 0)
+  ) %>% 
+  arrange(adm2_code, wps_input, month_dependent) %>% 
+  group_by(adm2_code, wps_input) %>% 
+  mutate(
+    fatalities_12m = rollsum(fatalities, 12, align = "right", fill = NA),
+    count_12m = rollsum(count, 12, align = "right", fill = NA),
+    binary_12m = rollmax(binary, 12, align = "right", fill = NA)
+  ) %>% 
+  ungroup()
+
+acled_indep_fatalities_wide <- acled_indep_full %>% 
+  select(iso3c:wps_input, fatalities_12m) %>% 
+  mutate(wps_input = paste0(wps_input, "_fatalities_12months")) %>% 
+  spread(wps_input, fatalities_12m)
+
+acled_indep_count_wide <- acled_indep_full %>% 
+  select(iso3c:wps_input, count_12m) %>% 
+  mutate(wps_input = paste0(wps_input, "_count_12months")) %>% 
+  spread(wps_input, count_12m)
+
+acled_indep_binary_wide <- acled_indep_full %>% 
+  select(iso3c:wps_input, binary_12m) %>% 
+  mutate(wps_input = paste0(wps_input, "_binary_12months")) %>% 
+  spread(wps_input, binary_12m)
+
+acled_wide_combined <- full_join(acled_indep_fatalities_wide, acled_indep_count_wide) %>% 
+  full_join(acled_indep_binary_wide)
+
+acled_all <- full_join(acled_full, acled_wide_combined)
+
+write_tsv(acled_all, "data/acled_all.txt.gz")
